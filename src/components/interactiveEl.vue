2d17fa1760de21db0fb76c032aeab9ae4efe9553
Heinrich Schmidt <40140403+Heinrich-Schmidt@users.noreply.github.com>
2022-11-17 15:18:13 +0300

    Update interactiveEl.vue

--- src/components/interactiveEl.vue
+++ src/components/interactiveEl.vue
@@ -1,7 +1,13 @@
 <template>
-    <div id="interactiveEl" :style="{ width: `${size[0]}px`, height: `${size[1]}px`, left: `${position[0]}px`, top: `${position[1]}px`, backgroundColor: `hsl(${position[0]-position[1]}, 70%, 60%)`}"></div>
+    <div id="interactiveEl" :style="{
+        width: `${size[0]}px`, 
+        height: `${size[1]}px`, 
+        left: `${position[0]}px`, 
+        top: `${position[1]}px`, 
+        backgroundColor: `hsl(${position[0]-position[1]}, 70%, 60%)`,
+        }">
+    </div>
 </template>
-  
 <script>
   export default {
     name: 'InteractiveEl',
